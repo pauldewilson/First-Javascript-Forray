849797595e02b44099b28d2ce5a2797cabf4b758
Paul <pauldewilson@gmail.com>
2020-10-27 10:50:57 +0000

    Added readme, minor changes

---
+++ README.md
@@ -0,0 +1,5 @@
+# First-JavaScript-Forray
+
+The first thing I have written in JavaScript having spent 3 days learning it.
+<br>
+The script creates a times-table array of `n*n` size and prints to console.
--- times_table.js
+++ times_table.js
@@ -1,7 +1,8 @@
 console.log(make_times_table(make_matrix(15)))
 
-// creates a times-table array 
 function make_times_table(matrix){
+	
+	// Amends a matrix to a times-table array 
 
     // for each row in the matrix, start at 1 since row 0 is the top (calculation) row
     for (var row=1; row < matrix.length; row++){
@@ -19,25 +20,25 @@ function make_matrix(size=5){
 
     // returns a matrix of arrays default 5x5
 
-    let init_list = []
+    let list_matrixes = []
 
     for (var i=0; i < size; i++){
         if (i == 0){
-            init_list[i] = make_array(size);
+            list_matrixes[i] = make_array(size);
         }
         else{
-            init_list[i] = make_zeros_array(size)
-            init_list[i][0] = init_list[0][i]
+            list_matrixes[i] = make_zeros_array(size)
+            list_matrixes[i][0] = list_matrixes[0][i]
         }
     }
-    return init_list
+    return list_matrixes
 }
 
 function make_zeros_array(arr_len){
 
     // returns an array of zeros
 
-    arr = []
+    let arr = []
 
     switch(typeof arr_len === 'number'){
         case false:
@@ -54,8 +55,9 @@ function make_array(arr_len){
     
     // returns array of numbers 0 to arr_len
     
-    arr = []
-    switch(typeof arr_len === 'number'){
+    let arr = []
+    
+	switch(typeof arr_len === 'number'){
         case false:
             console.log("ERR NUMERICAL ARRAY: A number wasn't passed, exiting");
             return 0;
